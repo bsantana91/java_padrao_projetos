5fc47b3a530275482968fc0ff714f321cad7e842
Bruno Santana <santana.b91@gmail.com>
2022-07-19 00:22:47 -0300

    Upload Inicial

---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/strategy/ComportamentoAgressivo.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.gof.strategy;
+
+public class ComportamentoAgressivo implements Comportamento {
+
+	@Override
+	public void mover() {
+		System.out.println("Robô está se movendo agressivamente");
+		
+	}
+	
+	
+	
+
+}
---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/strategy/ComportamentoNormal.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.gof.strategy;
+
+public class ComportamentoNormal implements Comportamento {
+
+	@Override
+	public void mover() {
+		System.out.println("Robô está se movendo normalmente");
+		
+	}
+	
+	
+	
+
+}
